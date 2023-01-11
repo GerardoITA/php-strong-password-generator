05ffbc759d1d5b9bad626f9269a1094dd4369ddb
GerardoITA <gerardo.giusti@tiscali.it>
2023-01-11 14:44:40 +0100

    creando markup statico e creato css ( incluso su php )

--- index.php
+++ index.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    <?php include 'style.css'; ?>
+    </style>    
     <title>Strong Password Generator</title>
 </head>
 <body>
-    
+    <div class="container">
+
+        <h1>Strong password generator</h1>
+        <h3>Genera una password sicura</h3>
+        <div class="warning">
+
+        </div>
+        <div class="form">
+
+        </div>
+    </div>
 </body>
 </html>
