9d0316e7b827f0650b417b90e40226fae3d219be
GerardoITA <gerardo.giusti@tiscali.it>
2023-01-12 09:48:06 +0100

    WIP

--- index.php
+++ index.php
@@ -24,7 +24,9 @@
         </div>
         <form class="form" method="POST">
             <div id="form_length">
+                <label for="passLength">Lunghezza password:</label>
                 <input type="text" name="passlength">
+                <input type="submit" value="Genera">
             </div>
             <div id="form_parameters">
                 
@@ -33,6 +35,24 @@
                 
             </div>
         </form>
+        <div class="pass"
+        <?
+        if ($passwordLength != false){
+            function generaPassword($lunghezza) {
+            for ( $i = 0; $i < $lunghezza; $i++ ){
+                $pass = '';
+                $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=';
+                $character = rand(0, $lunghezza);
+                $pass += $characters[$character];
+            }
+            return $pass;
+            };
+        }
+        ?>
+        >
+
+        </div>
+        
     </div>
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,3 +1,19 @@
 <?php
-$passwordLength = $_POST['passlength'];
+$passwordLength = $_POST['passlength'] ?? false;
+
+if ($passwordLength != false) {
+    function generaPassword($lunghezza) {
+    for ( $i = 0; $i < $lunghezza; $i++ ){
+        $pass = '';
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=';
+        $character = rand(0, $lunghezza);
+        $pass += $characters[$character];
+    }
+    return $pass;
+};
+
+}
+
+
+
 ?>
